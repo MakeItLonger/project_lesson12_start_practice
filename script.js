66366a1ba6dd63eb64e00df503e6e9ca6c345306
Vadym <zavadiuk@gmail.com>
2020-10-17 15:12:14 +0300

    Changes with Ivan, lesson 12

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-let personalMovieDB = {
+const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
@@ -8,15 +8,15 @@ let personalMovieDB = {
     private: false
 }
 
-let lastMovieWatch_1 = prompt('Один из последних просмотренных фильмов?', '');
-let lastMovieWatchRating_1 = prompt('На сколько оцените его?', '');
-let lastMovieWatch_2 = prompt('Один из последних просмотренных фильмов?', '');
-let lastMovieWatchRating_2 = prompt('На сколько оцените его?', '');
+const lastMovieWatch_1 = prompt('Один из последних просмотренных фильмов?', '');
+const lastMovieWatchRating_1 = prompt('На сколько оцените его?', '');
+const lastMovieWatch_2 = prompt('Один из последних просмотренных фильмов?', '');
+const lastMovieWatchRating_2 = prompt('На сколько оцените его?', '');
 
-let movies = {
-    lastMovieWatch_1: lastMovieWatchRating_1,
-    lastMovieWatch_2: lastMovieWatchRating_2
-}
+personalMovieDB.movies[lastMovieWatch_1] = lastMovieWatchRating_1;
+personalMovieDB.movies[lastMovieWatch_2] = lastMovieWatchRating_2;
+
+console.log(personalMovieDB);
 
 
 
